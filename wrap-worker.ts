e497a10e9502cc21fc145b323c3c1cc8703606f0
luckydye <tim.h4vlicek@gmail.com>
2022-12-12 01:07:59 +0100

    use spacees

--- wrap-worker.ts
+++ wrap-worker.ts
@@ -1,49 +1,49 @@
 function isWorker(path: string) {
-	if (path && path.match(/\.worker/g)) {
-		return true;
-	} else if (path === undefined) {
-		return true;
-	}
-	return false;
+  if (path && path.match(/\.worker/g)) {
+    return true;
+  } else if (path === undefined) {
+    return true;
+  }
+  return false;
 }
 
 export default function wrapWorker() {
-	const stack: string[] = [];
+  const stack: string[] = [];
 
-	return {
-		name: "wrap-workers",
+  return {
+    name: "wrap-workers",
 
-		transform(src: string, id: string) {
-			let code = src;
+    transform(src: string, id: string) {
+      let code = src;
 
-			if (!id.match("node_modules")) {
-				stack.unshift(id);
+      if (!id.match("node_modules")) {
+        stack.unshift(id);
 
-				const prev_file = stack[1];
+        const prev_file = stack[1];
 
-				if (!isWorker(prev_file) && isWorker(id)) {
-					code = `
+        if (!isWorker(prev_file) && isWorker(id)) {
+          code = `
             import * as Comlink from "comlink";
             const worker = new Worker(new URL("${id}", import.meta.url), { type: "module" });
             export default Comlink.wrap(worker);
           `;
-				} else {
-					if (isWorker(id)) {
-						code = `
+        } else {
+          if (isWorker(id)) {
+            code = `
               import * as Comlink from "comlink";
               ${code.replace("export default", "export const mod = ")}
               Comlink.expose(mod);
             `;
 
-						console.log(id, code);
-					}
-				}
-			}
+            console.log(id, code);
+          }
+        }
+      }
 
-			return {
-				code,
-				map: null, // provide source map if available
-			};
-		},
-	};
+      return {
+        code,
+        map: null, // provide source map if available
+      };
+    },
+  };
 }
