eb477e85a21147beccc7983b08bd065fd1b38939
luckydye <tim.h4vlicek@gmail.com>
2022-12-12 01:14:41 +0100

    build

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    outDir: "lib",
+    lib: {
+      entry: "wrap-worker.ts",
+      formats: ["es"],
+    },
+    emptyOutDir: true,
+  },
+});
