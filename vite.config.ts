90bf30ce1dd2a01cc893a48cbc51b32f2762d35f
Tim Havlicek <t.havlicek@s-v.de>
2022-12-14 10:25:27 +0100

    external deps

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,16 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
+    target: "esnext",
+    minify: false,
     outDir: "lib",
     lib: {
       entry: "wrap-worker.ts",
       formats: ["cjs"],
     },
     emptyOutDir: true,
+    rollupOptions: {
+      external: ["node:crypto"],
+    },
   },
 });
